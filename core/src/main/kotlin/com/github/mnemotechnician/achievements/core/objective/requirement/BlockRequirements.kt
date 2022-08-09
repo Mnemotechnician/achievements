1275234ec26e7163f087aced1e6792bb9f079f4a
Mnemotechnician <daytimer253@gmail.com>
2022-08-09 15:29:28 +0300

    New requirements

--- core/src/main/kotlin/com/github/mnemotechnician/achievements/core/objective/requirement/BlockRequirements.kt
+++ core/src/main/kotlin/com/github/mnemotechnician/achievements/core/objective/requirement/BlockRequirements.kt
@@ -1,11 +1,17 @@
 package com.github.mnemotechnician.achievements.core.objective.requirement
 
+import arc.struct.Seq
 import com.github.mnemotechnician.achievements.core.objective.AbstractCounterObjective.Requirement
 import com.github.mnemotechnician.achievements.core.objective.event.ObjectiveEvent
 import com.github.mnemotechnician.achievements.core.objective.event.ObjectiveEvents.BuildingEvent
 import com.github.mnemotechnician.achievements.core.util.emojiOrName
-import com.github.mnemotechnician.mkui.delegates.dynamicBundle
+import com.github.mnemotechnician.achievements.core.util.int
+import com.github.mnemotechnician.mkui.delegates.bundle
+import mindustry.type.Item
 import mindustry.world.Block
+import mindustry.world.Tile
+import mindustry.world.blocks.environment.Floor
+import mindustry.world.blocks.production.Drill
 
 /**
  * Requires a block in proximity of the block that's being placed or removed.
@@ -18,9 +24,9 @@ class ProximityRequirement(
 	vararg val blocks: Block
 ) : Requirement("proximity") {
 	val blockNames = blocks.joinToString(", ") { it.emojiOrName() }
-	override val description by dynamicBundle(bundlePrefix, { if (allOf) 1 else 0 }, { blockNames })
+	override val description by bundle(bundlePrefix, { allOf.int }, { blockNames })
 
-	/** Same as primary but [allOf] is true. */
+	/** Same as the primary constructor. */
 	constructor(vararg blocks: Block) : this(true, *blocks)
 
 	override fun isAccepted(event: ObjectiveEvent) = when (event) {
@@ -34,4 +40,59 @@ class ProximityRequirement(
 		else -> true
 	}
 }
-// todo add more
+
+/**
+ * Requires the player to build blocks on a specific overlay or floor.
+ *
+ * @param overlay if true, the floor must be an overlay floor.
+ * @param allOf if true, the block must stand on each of the specified floors/overlays. Otherwise, at least on one.
+ */
+class FloorRequirement(
+	val overlay: Boolean = false,
+	val allOf: Boolean = true,
+	vararg val floors: Floor
+) : Requirement("floor") {
+	val blockNames = floors.joinToString(", ") { it.emojiOrName() }
+	override val description by bundle(bundlePrefix, { allOf.int }, { blockNames })
+
+	/** Same as the primary constructor. */
+	constructor(overlay: Boolean = false, vararg floors: Floor) : this(overlay, true, *floors)
+
+	override fun isAccepted(event: ObjectiveEvent) = if (event is BuildingEvent) {
+		if (allOf) {
+			val occupied = event.building.tile.getLinkedTiles(tmpSeq)
+
+			floors.all {  floor ->
+				occupied.any {
+					(if (overlay) it.overlay() else it.floor()) == floor
+				}
+			}
+		} else {
+			var result = false
+			event.building.tile.getLinkedTiles {
+				val floor = if (overlay) it.overlay() else it.floor()
+				if (floor in floors) result = true
+			}
+			result
+		}
+	} else true
+
+	companion object {
+		private val tmpSeq = Seq<Tile>()
+	}
+}
+
+/**
+ * A drill-specific requirement, which requires the player to make the drills mine a specific item.
+ * Has no effect on non-drill blocks.
+ */
+class MiningRequirement(val minedItem: Item) : Requirement("mining") {
+	override val description by bundle(bundlePrefix, { minedItem.emojiOrName() })
+
+	override fun isAccepted(event: ObjectiveEvent) = if (event is BuildingEvent) {
+		val build = event.building
+		if (build is Drill.DrillBuild) {
+			build.dominantItem == minedItem
+		} else true
+	} else true
+}
